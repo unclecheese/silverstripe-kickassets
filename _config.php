cf06d60d908c04d4fbfdd70957d8824417f2be2b
Garion Herman <garion@silverstripe.com>
2016-02-05 23:11:14 +1300

    Added check to make sure User Agent is set.

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
 define('KICKASSETS_DIR', basename(__DIR__));
 
 // IE9 doesn't get to play with us.
-if(!preg_match('/(?i)msie [5-9]/',$_SERVER['HTTP_USER_AGENT'])) {
+if(isset($_SERVER['HTTP_USER_AGENT']) && !preg_match('/(?i)msie [5-9]/',$_SERVER['HTTP_USER_AGENT'])) {
 	CMSMenu::remove_menu_item('AssetAdmin');
 }
 else {
